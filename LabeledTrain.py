005a97636c153166010642d6c39f0082bb7f6fd5
TangChangXin <592609672@qq.com>
2022-04-29 22:56:06 +0800

    有标签训练

--- LabeledTrain.py
+++ LabeledTrain.py
@@ -10,10 +10,12 @@ from torch.backends.cudnn import deterministic
 def 训练模型(网络模型, 优化器, 预训练权重路径, 硬件设备, 命令行参数, 随机图像变换):
     # 加载训练数据集和测试数据集
     数据集根路径 = "LabeledDataset"
-    有标签训练数据集 = datasets.ImageFolder(root=os.path.join(数据集根路径, "Train"), transform=随机图像变换["测试集"])
+    有标签训练数据集 = datasets.ImageFolder(root=os.path.join(数据集根路径, "Train"), transform=随机图像变换["训练集"])
+    # 训练类列表 = 有标签训练数据集.class_to_idx
     有标签训练数据 = torch.utils.data.DataLoader(有标签训练数据集, batch_size=命令行参数.labeled_data_batch_size, shuffle=True,
                                           num_workers=0, pin_memory=True)
     有标签测试数据集 = datasets.ImageFolder(root=os.path.join(数据集根路径, "Validate"), transform=随机图像变换["测试集"])
+    # 测试类列表 = 有标签测试数据集
     有标签测试数据 = torch.utils.data.DataLoader(有标签测试数据集, batch_size=命令行参数.labeled_data_batch_size, shuffle=False,
                                           num_workers=0, pin_memory=True)
     assert os.path.exists(预训练权重路径), "自监督预训练模型权重{}不存在.".format(预训练权重路径)
@@ -21,7 +23,7 @@ def 训练模型(网络模型, 优化器, 预训练权重路径, 硬件设备, �
     网络模型.to(硬件设备)
     训练损失函数 = torch.nn.CrossEntropyLoss()
     测试损失函数 = torch.nn.CrossEntropyLoss()
-    学习率调整器 = torch.optim.lr_scheduler.ReduceLROnPlateau(优化器, mode='max', factor=0.5, patience=10, cooldown=10)
+    学习率调整器 = torch.optim.lr_scheduler.ReduceLROnPlateau(优化器, mode='min', factor=0.5, patience=30, cooldown=10)
     最高测试准确率 = 0.0
     # 开始训练
     for 当前训练周期 in range(1, 命令行参数.labeled_train_max_epoch + 1):
@@ -29,7 +31,7 @@ def 训练模型(网络模型, 优化器, 预训练权重路径, 硬件设备, �
         当前周期全部训练损失 = 0.0
         训练正确总数目 = 0
         # 每一批数据训练。enumerate可以在遍历元素的同时输出元素的索引
-        训练循环 = tqdm(enumerate(有标签训练数据), total=len(有标签训练数据), ncols=130, leave=True)
+        训练循环 = tqdm(enumerate(有标签训练数据), total=len(有标签训练数据), ncols=100, leave=True)
         for 当前批次, (图像数据, 标签) in 训练循环:
             图像数据, 标签 = 图像数据.to(硬件设备), 标签.to(硬件设备)
             训练集预测概率 = 网络模型(图像数据)
@@ -42,8 +44,9 @@ def 训练模型(网络模型, 优化器, 预训练权重路径, 硬件设备, �
             预测类别 = torch.max(训练集预测概率, dim=1)[1]
             训练正确总数目 += torch.eq(预测类别, 标签).sum().item()  # 累加每个批次预测正确的数目
             当前迭代周期训练准确率 = 训练正确总数目 / len(有标签训练数据集)
-            训练循环.desc = "训练周期 [{}/{}] 训练损失：{:.8f} 准确率{:.8f}".format(当前训练周期, 命令行参数.labeled_train_max_epoch,
-                                                            训练损失.detach().item(), 当前迭代周期训练准确率)  # 设置进度条描述
+            训练循环.set_description(f"训练周期 [{当前训练周期}/{命令行参数.labeled_train_max_epoch}] 训练损失：{训练损失.detach().item():.5f} 准确率{当前迭代周期训练准确率:.5f}") # 设置进度条描述
+            # 训练循环.set_description(f"训练周期 [{当前训练周期}/{命令行参数.labeled_train_max_epoch}]")
+            # 训练循环.set_postfix(训练损失 = 训练损失.detach().item(), 准确率 = 当前迭代周期训练准确率)
 
         学习率调整器.step(当前周期全部训练损失)  # 调整学习率
         # 记录每个周期训练损失值
@@ -55,7 +58,7 @@ def 训练模型(网络模型, 优化器, 预训练权重路径, 硬件设备, �
         测试正确的总数目 = 0
         # 下方代码块不反向计算梯度
         with torch.no_grad():
-            测试循环 = tqdm(enumerate(有标签测试数据), total=len(有标签测试数据), ncols=130, leave=True)
+            测试循环 = tqdm(enumerate(有标签测试数据), total=len(有标签测试数据), ncols=100, leave=True)
             for 当前批次, (图像数据, 标签) in 测试循环:
                 图像数据, 标签 = 图像数据.to(硬件设备), 标签.to(硬件设备)
                 测试集预测概率 = 网络模型(图像数据)
@@ -65,7 +68,7 @@ def 训练模型(网络模型, 优化器, 预训练权重路径, 硬件设备, �
                 预测类别 = torch.max(测试集预测概率, dim=1)[1]
                 测试正确的总数目 += torch.eq(预测类别, 标签).sum().item()  # 累加每个批次预测正确的数目
                 当前迭代周期测试准确率 = 测试正确的总数目 / len(有标签测试数据集)
-                测试循环.desc = "测试周期 [{}/{}] 测试损失：{:.8f} 准确率{:.8f}".format(当前训练周期, 命令行参数.labeled_train_max_epoch, 测试损失.detach().item(), 当前迭代周期测试准确率)  # 设置进度条描述
+                测试循环.desc = "测试周期 [{}/{}] 测试损失：{:.5f} 准确率{:.5f}".format(当前训练周期, 命令行参数.labeled_train_max_epoch, 测试损失.detach().item(), 当前迭代周期测试准确率)  # 设置进度条描述
         # 当前迭代周期测试准确率 = 测试正确的总数目 / len(有标签测试数据集)
 
         # 记录每个周期测试损失值
@@ -115,7 +118,6 @@ def 有标签训练(命令行参数):
             transforms.RandomHorizontalFlip(p=0.5),  # 随机水平翻转
             # 修改亮度、对比度和饱和度
             transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),  # 随机应用添加的各种图像变换
-            transforms.RandomGrayscale(p=0.2),  # todo 随机灰度化，但我本来就是灰度图啊
             transforms.ToTensor(),  # 转换为张量且维度是[C, H, W]
             # 三通道归一化
             transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])]),
@@ -126,23 +128,9 @@ def 有标签训练(命令行参数):
     }
 
     # 优化器 = torch.optim.Adam(分类模型.全连接.parameters(), lr=1e-3, weight_decay=1e-6) # 只训练分类层
-    分类模型1 = SimCLRModel.有监督simCLRresnet50(2)
+    分类模型1 = SimCLRModel.有监督simCLRresnet50(3)
     优化器1 = torch.optim.Adam(分类模型1.parameters())
-    训练模型(分类模型1, 优化器1, "Weight/OCTA(FULL)AdamunlabeledBest_model.pth",硬件设备, 命令行参数, 随机图像变换)
-
-    # 分类模型2 = SimCLRModel.有监督simCLRresnet50(2)
-    # 优化器2 = torch.optim.Adam(分类模型2.parameters())
-    # 训练模型(分类模型2, 优化器2, 硬件设备, 命令行参数, 随机图像变换)
-
-    # 分类模型3 = SimCLRModel.有监督simCLRresnet50(2)
-    # 优化器3 = torch.optim.Adam(分类模型3.parameters())
-    # 训练模型(分类模型3, 优化器3, 硬件设备, 命令行参数, 随机图像变换)
-
-    # 分类模型4 = SimCLRModel.有监督simCLRresnet50(2)
-    # 优化器4 = torch.optim.Adam(分类模型4.parameters())
-    # 训练模型(分类模型4, 优化器4, 硬件设备, 命令行参数, 随机图像变换)
-
-    # 开始训练
+    训练模型(分类模型1, 优化器1, "Weight/OCTA(FULL)AdamunlabeledBest_model.pth", 硬件设备, 命令行参数, 随机图像变换)
 
 
 if __name__ == '__main__':
@@ -151,7 +139,7 @@ if __name__ == '__main__':
 
     # 添加有标签数据训练时的参数
     命令行参数解析器.add_argument('--labeled_data_batch_size', default=1, type=int, help="有标签数据训练时的批量大小")
-    命令行参数解析器.add_argument('--labeled_train_max_epoch', default=2, type=int, help="有标签训练的最大迭代周期")
+    命令行参数解析器.add_argument('--labeled_train_max_epoch', default=500, type=int, help="有标签训练的最大迭代周期")
     命令行参数解析器.add_argument('--labeled_train_resize', default=224, type=int, help="随机缩放图像的大小")
 
     # 获取命令行传入的参数
